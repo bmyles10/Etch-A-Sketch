981c11550fc36a016f8e2b874866cf0e539cc0ac
Brandon Myles <bmyles1006@gmail.com>
2019-04-26 10:32:49 -0500

    Version 1.00 of App

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+/*
+Need to create this to only start when you choose a button. I hated that you started the app and it just started making markings
 
-//Code below is how to create a grid - Look at https://zorafins.github.io/etch-a-sketch/
+*/
 
 let container = document.querySelector("#container");
 
@@ -8,12 +10,12 @@ let createGrid = (squaresPerSide) => {
         let square = document.createElement("div");
         square.classList.add("square");
         square.style.width = (750 / squaresPerSide - 2).toString() + 'px';
-        square.style.height = (750 / squaresPerSide - 2).toString() + 'px';        
+        square.style.height = (750 / squaresPerSide - 2).toString() + 'px';
         container.appendChild(square);
-        console.log(container);        
+
     }
 }
-
+//Creating a Random Color Function
 let getRandomColor = () => {
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -23,14 +25,101 @@ let getRandomColor = () => {
     return color;
 }
 
+
+
+//Function to change squares black
 let changeToBlack = (event) => {
     if (event.target !== event.currentTarget) {
         event.target.style.background = 'black';
     }
     event.stopPropagation();
 }
+//Function to change squares rainbow colors
+let changetoRainbow = (event) => {
+    if (event.target !== event.currentTarget) {
+        event.target.style.background = getRandomColor();
+    }
+    event.stopPropagation();
+}
 
-container.addEventListener("mouseover", changeToBlack);
+//Function to change squares white
+let changetoWhite = (event) => {
+    if (event.target !== event.currentTarget) {
+        event.target.style.background = 'white';
+    }
+    event.stopPropagation();
+}
 
-//Should show grid with 16 rows and 16 columns
-createGrid(16);
+
+
+//Function to reload page
+let reloadWindow = () => {
+    location.reload();
+}
+
+
+//Rainbow Color Event
+let colored = () => {
+    container.addEventListener("mouseover", changetoRainbow);
+
+}
+
+//Black Color Event
+let blacked = () => {
+    container.addEventListener("mouseover", changeToBlack);
+}
+
+//Eraser Event
+let eraser = () => {
+    container.addEventListener("mouseover", changetoWhite);
+}
+
+
+
+//Create button to restart, this should refresh the page entirely
+let reset = () => {
+    let button1 = document.createElement("button");
+    button1.textContent = "Reset";
+    let btn = document.getElementById("btn1");
+    btn.appendChild(button1)
+    button1.addEventListener("click", reloadWindow);
+    createGrid(16);
+}
+
+//Create button to do colors / Should remove this button and the black one after click
+let rainbowSketch = () => {
+    let button = document.createElement("button");
+    button.textContent = "Rainbow Colors";
+    let btn = document.getElementById("btn")
+    btn.appendChild(button);
+    button.addEventListener("click", colored);
+    createGrid(16); //Will need to change the argument to a varible that ask user how many cells to use.
+
+}
+
+//Create button to do just black / Should remove this button and the color one after click
+let blackSketch = () => {
+    let button2 = document.createElement("button");
+    button2.textContent = "Regular Black";
+    let btn2 = document.getElementById("btn2")
+    btn2.appendChild(button2);
+    button2.addEventListener("click", blacked);
+    createGrid(16); //Will need to change the argument to a varible that ask user how many cells to use.
+}
+
+//Create Eraser
+let erase = () => {
+    let button = document.createElement("button");
+    button.textContent = "Eraser";
+    let btn = document.getElementById("btn3");
+    btn.appendChild(button);
+    button.addEventListener("click", eraser);
+    createGrid(16); //Will need to change the argument to a varible that ask user how many cells to use.
+}
+
+
+
+rainbowSketch();
+blackSketch();
+reset();
+erase();
